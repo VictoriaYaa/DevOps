ce28334936c9d0efb1e48b03dd01e77e75d26154
Victoria Yaakov <victoriaja1916@gmail.com>
2023-05-28 16:24:57 +0300

    adding scripts

---
+++ Parsering-logs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+LOG_FILE="/Users/victoriayaakov/Documents/Repos/DevOps/access-log.log"
+
+# # Per IP
+cut -d ' ' -f1 access-log.log | sort -n | uniq -c | sort -nr | head -20 > "access-log.log.2"
+cat access-log.log.2
+rm access-log.log.2
+# Per Date
+grep -Eo '[0-9]{1,2}/[A-Za-z]{3}/[0-9]{4}' "$LOG_FILE" | awk '!seen[$0]++' | sort -n | uniq -c | sort -nr | head -20 > "access-log.log.3"
+# Display the formatted log file
+cat access-log.log.3
+rm access-log.log.3
+
+
