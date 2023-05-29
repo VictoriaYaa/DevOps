339d4a76fb7d3f4d1bca1e586c5cb351168c1a4d
Victoria Yaakov <victoriaja1916@gmail.com>
2023-05-29 21:15:19 +0300

    remmoved spaces

--- Parsering-logs.sh
+++ Parsering-logs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 LOG_FILE="/Users/victoriayaakov/Documents/Repos/DevOps/access-log.log"
-
-# # Per IP
+# Per IP
 cut -d ' ' -f1 access-log.log | sort -n | uniq -c | sort -nr | head -20 > "access-log.log.2"
 cat access-log.log.2
 rm access-log.log.2
 # Per Date
-grep -Eo '[0-9]{1,2}/[A-Za-z]{3}/[0-9]{4}' "$LOG_FILE" | awk '!seen[$0]++' | sort -n | uniq -c | sort -nr | head -20 > "access-log.log.3"
-# Display the formatted log file
+
+grep -Eo '[0-9]{1,2}/[A-Za-z]{3}/[0-9]{4}' $LOG_FILE | sort | uniq -c | sort -nr > access-log.log.3
+# # Display the formatted log file
 cat access-log.log.3
 rm access-log.log.3
 
